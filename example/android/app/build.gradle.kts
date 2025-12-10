cafbdf097e24bc36ef815cf559b986c23374f456
Tanh Nguyen <nvtanh.dev@gmail.com>
2025-12-10 19:37:19 +0700

    feat: update java and kotlin compatibility to version 17

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -11,12 +11,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // Match Kotlin 17 toolchain
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
